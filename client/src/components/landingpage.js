cce1228dbf1572563acf34b28a4a1f958d5f226c
Aref Smiley <aref.smiley@yahoo.com>
2020-10-18 21:31:29 -0400

    added footer

--- client/src/components/landingpage.js
+++ client/src/components/landingpage.js
@@ -6,6 +6,7 @@ import Cards from '../components/Cards';
 import Cards2 from '../components/Cards2';
 import Cards3 from '../components/Cards3';
 import Cards4 from '../components/Cards4';
+import Footer from '../components/Footer';
 
 
 class Landing extends React.Component {
@@ -18,19 +19,7 @@ class Landing extends React.Component {
         <Cards4 />
         <Cards />
         <Cards2 />
-        
-
-
-
-
-        
-
-
-
-
-        <h3>Your Career Upgrade Starts Here.....</h3>
-        <Link to="/register"><Button>Register</Button></Link>
-        <Link to="/login">Already a member? Click to log in.</Link>
+        <Footer />
       </div>
     );
   }
